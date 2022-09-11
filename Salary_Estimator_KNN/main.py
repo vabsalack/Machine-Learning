b7d20bad5b7efd7b3c6b1318f255df4d56558387
kittuvasa <kittu.black.space@gmail.com>
2022-09-11 13:41:14 +0530

    KNNeighborsClassifier salaryEstimation upload

--- Salary_Estimator_KNN/main.py
+++ Salary_Estimator_KNN/main.py
@@ -7,10 +7,21 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 
+from sklearn.metrics import confusion_matrix, accuracy_score
+
 
 def load_dataset(filename):
     ds = pd.read_csv(filename)
     return ds
+
+
+def calculate_cm(model, x_test, ytest):
+    ypred = model.predict(x_test)
+    cm = confusion_matrix(ytest, ypred)
+    print("Confusion Matrix: ")
+    print(cm)
+
+    print(f"Accuracy of the model: {accuracy_score(ytest, ypred)*100}")
 
 
 def model_train(data):
@@ -32,20 +43,20 @@ def model_train(data):
         model.fit(xtr, ytr)
         prediction_i = model.predict(xte)
         error.append((i, np.mean(prediction_i != yte)))
-    error1 = [i[1] for i in error]
-
-    plt.plot(range(1, 41),
-             error1,
-             color="red",
-             linestyle="dashed",
-             marker="o",
-             markerfacecolor="blue",
-             markersize=10)
-
-    plt.title("Error rate of k[1 - 41] value")
-    plt.xlabel("K value")
-    plt.ylabel("Mean Error")
-    plt.show()
+    # error1 = [i[1] for i in error]
+    #
+    # plt.plot(range(1, 41),
+    #          error1,
+    #          color="red",
+    #          linestyle="dashed",
+    #          marker="o",
+    #          markerfacecolor="blue",
+    #          markersize=10)
+    #
+    # plt.title("Error rate of k[1 - 41] value")
+    # plt.xlabel("K value")
+    # plt.ylabel("Mean Error")
+    # plt.show()
 
     i = sorted(error, key=lambda z: z[1])[0][0]
     model = KNeighborsClassifier(n_neighbors=i,
@@ -55,7 +66,7 @@ def model_train(data):
 
     model.fit(xtr, ytr)
 
-    return model, sc
+    return model, sc, xte, yte
 
 
 def predict_data(model, sc):
@@ -76,12 +87,14 @@ def main():
     data["income"] = data["income"].map({"<=50K": 1, ">50K": 0}).astype(int)
     print(data.head)
 
-    model, sc = model_train(data)
+    model, sc, xte, yte = model_train(data)
 
-    if predict_data(model, sc):
-        print("Employee might not got  Salary above 50K")
-    else:
-        print("Employee might got  Salary above 50K")
+    # if predict_data(model, sc):
+    #     print("Employee might not got  Salary above 50K")
+    # else:
+    #     print("Employee might got  Salary above 50K")
+
+    calculate_cm(model, xte, yte)
 
 
 if __name__ == "__main__":
